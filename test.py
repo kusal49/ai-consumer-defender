e91b8cd1eaaf5d10041696019ad5638a88f53a18
Kusal <kusal@Kusals-MacBook-Air.local>
2026-01-20 22:58:40 +0530

    Delete test.py

--- test.py
+++
@@ -1,5 +0,0 @@
-import langchain
-from langchain.agents import create_tool_calling_agent
-
-print(f"LangChain Version: {langchain.__version__}")
-print("SUCCESS: create_tool_calling_agent is found!")
